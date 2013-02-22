565b4ea4f801e5d298e7d2b2ba1e4ccee6e5000c
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-02-22 16:05:19 -0500

    Setup middleman for mockups

---
+++ mockups/config.rb
@@ -0,0 +1,13 @@
+
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :build_dir, '../public/mockups'
+
+# Build-specific configuration
+configure :build do
+end
